980b058f888c25e1d7a525fb1e22312b85286385
smnkumarpaul <smnkumarpaul@gmail.com>
2021-12-12 19:30:48 +0530

    models registered

---
+++ backend/models/index.js
@@ -0,0 +1,13 @@
+const AuthUsersModel = require('./AuthUsers.modell')
+const CustomersModel = require('./Customer.model')
+const AdminModel = require('./Admin.model')
+const MoviesModel = require('./Movies.model')
+const GenresModel = require('./Genres.model')
+
+module.exports = {
+  authUsersModel: AuthUsersModel,
+  customersModel: CustomersModel,
+  adminModel: AdminModel,
+  moviesModel: MoviesModel,
+  genresModel: GenresModel
+}
