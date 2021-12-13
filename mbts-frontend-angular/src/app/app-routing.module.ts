5095ef9bc12ef931ad0d63d8193cc9f9d04f5789
smnkumarpaul <smkumarpaul@gmail.com>
2021-12-13 06:02:19 +0530

    application moduleupdated with UI modules

--- mbts-frontend-angular/src/app/app-routing.module.ts
+++ mbts-frontend-angular/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesHomepageComponent } from './movies-homepage/movies-homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesHomepageComponent
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin-module/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mbts-frontend-angular/src/app/app.module.ts
+++ mbts-frontend-angular/src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { PopupformContainerComponent } from './popupform-container/popupform-container.component';
+import { PopupformComponent } from './popupform/popupform.component';
+import { AdminModule } from "./admin-module/admin.module";
+import { MovieListModule } from './movie-list/movie-list.module';
+import { MoviesHomepageComponent } from './movies-homepage/movies-homepage.component';
+import { AdminMovieListComponent } from './admin-module/admin-movie-list/admin-movie-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PopupformContainerComponent,
+    PopupformComponent,
+    MoviesHomepageComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    AdminModule,
+    MovieListModule
   ],
-  providers: [],
+  providers: [
+    NgbModal
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
